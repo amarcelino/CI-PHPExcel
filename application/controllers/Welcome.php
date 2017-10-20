c154c1fc2649d2404b341295d48d0c1f52a043eb
amarcelino <marcelinoandre@gmail.com>
2017-10-20 00:01:15 -0200

    add array excel
    x

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -38,4 +38,42 @@ class Welcome extends CI_Controller {
 		//echo 'planilha Gerada com sucesso !  :)';
 
 	}
+    
+	
+	
+	public function excelArray( $param ) {
+        // Here is the sample array of data
+        $data = array(
+            array( 'name' => 'A', 'mail' => 'a@gmail.com', 'age' => 43 ),
+            array( 'name' => 'C', 'mail' => 'c@gmail.com', 'age' => 24 ),
+            array( 'name' => 'B', 'mail' => 'b@gmail.com', 'age' => 35 ),
+            array( 'name' => 'G', 'mail' => 'f@gmail.com', 'age' => 22 ),
+            array( 'name' => 'F', 'mail' => 'd@gmail.com', 'age' => 52 ),
+            array( 'name' => 'D', 'mail' => 'g@gmail.com', 'age' => 32 ),
+            array( 'name' => 'E', 'mail' => 'e@gmail.com', 'age' => 34 ),
+            array( 'name' => 'K', 'mail' => 'j@gmail.com', 'age' => 18 ),
+            array( 'name' => 'L', 'mail' => 'h@gmail.com', 'age' => 25 ),
+            array( 'name' => 'H', 'mail' => 'i@gmail.com', 'age' => 28 ),
+            array( 'name' => 'J', 'mail' => 'j@gmail.com', 'age' => 53 ),
+            array( 'name' => 'I', 'mail' => 'l@gmail.com', 'age' => 26 ),
+        );
+
+
+        $objPHPExcel = new PHPExcel();
+        // Fill worksheet from values in array
+        $objPHPExcel->getActiveSheet()->fromArray( $data, null, 'A2' );
+
+        // Rename worksheet
+        $objPHPExcel->getActiveSheet()->setTitle( 'enois' );
+
+        // Set AutoSize for name and email fields
+        $objPHPExcel->getActiveSheet()->getColumnDimension( 'A' )->setAutoSize( true );
+        $objPHPExcel->getActiveSheet()->getColumnDimension( 'B' )->setAutoSize( true );
+
+
+        // Save Excel 2007 file
+        $objWriter = PHPExcel_IOFactory::createWriter( $objPHPExcel, 'Excel2007' );
+        $objWriter->save( 'MyExcel.xls' );
+    }	
+	
 }
